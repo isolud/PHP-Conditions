1384fa165aa328b0a05c9816a9b799c2b587ad01
isolud <simonstien85@gmail.com>
2019-06-12 00:02:15 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sstienface
+ * Date: 04/12/2018
+ * Time: 11:25
+ */
+
+// Premiere ligne
+$a = 50;
+$b = 10;
+       >
+{
+    echo "Hello World!";
+}
+
+//Deuxieme ligne
+
+{
+    echo "<br><br>Hello World2";
+}
+
+//Troisieme ligne
+($a==b)
+{
+    echo "<br><br>Oui";
+}
+
+{
+    echo "<br><br>Non";
+}
+
+//Quatrieme ligne
+echo"<br><br>";
+ ($a == $b) {
+    echo "1";
+}  ($a > $b) {
+    echo "2";
+}  {
+    echo "3";
+}
+
+
+//Cinquieme ligne
+echo"<br><br>";
+
+ ($color) {
+    "red":
+    echo "Hello";
+   "green":
+    echo "Welcome";
+}
+
